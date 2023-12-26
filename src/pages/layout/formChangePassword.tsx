05b42f1e9dbadc6d42aaae31001669d718d0cba0
dungtientran <dungcq86@gmail.com>
2023-12-26 10:32:44 +0700

    qua app mà đổi

--- src/pages/layout/formChangePassword.tsx
+++ src/pages/layout/formChangePassword.tsx
@@ -1,6 +1,8 @@
-import { Button, Form, Input, Spin } from 'antd';
+import { Button, Form, Input, Space, Spin, Typography } from 'antd';
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+
+const { Title, Text } = Typography;
 
 const passwordValidator = (value: string): boolean => {
   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\^$*.\[\]{}\(\)?\-"!@#%&/,><\':;|_~`])\S{7,99}$/;
@@ -26,103 +28,106 @@ const FormChangePassword = () => {
   }, [user]);
 
   return (
-    <Form
-      form={form}
-      name="register"
-      layout="vertical"
-      autoComplete="off"
-      onFinish={onFinish}
-      style={{ maxWidth: 600 }}
-      scrollToFirstError
-    >
-      <Form.Item name="email" label="Email">
-        <Input disabled />
-      </Form.Item>
+    <Space>
+      <Title level={1}>Vui lòng chuyển qua app để đổi mật khẩu !</Title>
+    </Space>
+    // <Form
+    //   form={form}
+    //   name="register"
+    //   layout="vertical"
+    //   autoComplete="off"
+    //   onFinish={onFinish}
+    //   style={{ maxWidth: 600 }}
+    //   scrollToFirstError
+    // >
+    //   <Form.Item name="email" label="Email">
+    //     <Input disabled />
+    //   </Form.Item>
 
-      <Form.Item
-        name="oldPassword"
-        label="Mật khẩu cũ"
-        rules={[
-          {
-            required: true,
-            message: 'Không được bỏ trống!',
-          },
-          {
-            validator(_, value) {
-              if (!value || passwordValidator(value)) {
-                return Promise.resolve();
-              }
+    //   <Form.Item
+    //     name="oldPassword"
+    //     label="Mật khẩu cũ"
+    //     rules={[
+    //       {
+    //         required: true,
+    //         message: 'Không được bỏ trống!',
+    //       },
+    //       {
+    //         validator(_, value) {
+    //           if (!value || passwordValidator(value)) {
+    //             return Promise.resolve();
+    //           }
 
-              return Promise.reject(
-                new Error(
-                  'Mật khẩu tối thiểu 8 ký tự và phải có ít nhất 1 chữ số, 1 chữ cái in hoa và 1 kí tự đặc biệt',
-                ),
-              );
-            },
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password placeholder="Mật khẩu cũ" />
-      </Form.Item>
-      <Form.Item
-        name="newPassword"
-        label="Mật khẩu mới"
-        rules={[
-          {
-            required: true,
-            message: 'Vui lòng nhập Mật khẩu!',
-          },
-          {
-            validator(_, value) {
-              if (!value || passwordValidator(value)) {
-                return Promise.resolve();
-              }
+    //           return Promise.reject(
+    //             new Error(
+    //               'Mật khẩu tối thiểu 8 ký tự và phải có ít nhất 1 chữ số, 1 chữ cái in hoa và 1 kí tự đặc biệt',
+    //             ),
+    //           );
+    //         },
+    //       },
+    //     ]}
+    //     hasFeedback
+    //   >
+    //     <Input.Password placeholder="Mật khẩu cũ" />
+    //   </Form.Item>
+    //   <Form.Item
+    //     name="newPassword"
+    //     label="Mật khẩu mới"
+    //     rules={[
+    //       {
+    //         required: true,
+    //         message: 'Vui lòng nhập Mật khẩu!',
+    //       },
+    //       {
+    //         validator(_, value) {
+    //           if (!value || passwordValidator(value)) {
+    //             return Promise.resolve();
+    //           }
 
-              return Promise.reject(
-                new Error(
-                  'Mật khẩu tối thiểu 8 ký tự và phải có ít nhất 1 chữ số, 1 chữ cái in hoa và 1 kí tự đặc biệt',
-                ),
-              );
-            },
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password placeholder="Mật khẩu mới" />
-      </Form.Item>
+    //           return Promise.reject(
+    //             new Error(
+    //               'Mật khẩu tối thiểu 8 ký tự và phải có ít nhất 1 chữ số, 1 chữ cái in hoa và 1 kí tự đặc biệt',
+    //             ),
+    //           );
+    //         },
+    //       },
+    //     ]}
+    //     hasFeedback
+    //   >
+    //     <Input.Password placeholder="Mật khẩu mới" />
+    //   </Form.Item>
 
-      <Form.Item
-        name="confirm"
-        label="Nhập lại mật khẩu mới"
-        dependencies={['newPassword']}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: 'Vui lòng nhập lại mật khẩu!',
-          },
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              if (!value || getFieldValue('newPassword') === value) {
-                return Promise.resolve();
-              }
+    //   <Form.Item
+    //     name="confirm"
+    //     label="Nhập lại mật khẩu mới"
+    //     dependencies={['newPassword']}
+    //     hasFeedback
+    //     rules={[
+    //       {
+    //         required: true,
+    //         message: 'Vui lòng nhập lại mật khẩu!',
+    //       },
+    //       ({ getFieldValue }) => ({
+    //         validator(_, value) {
+    //           if (!value || getFieldValue('newPassword') === value) {
+    //             return Promise.resolve();
+    //           }
 
-              return Promise.reject(new Error('Mật khẩu không trùng!'));
-            },
-          }),
-        ]}
-      >
-        <Input.Password placeholder="Nhập lại mật khẩu" />
-      </Form.Item>
-      <Form.Item style={{ marginTop: '' }}>
-        <Spin spinning={false}>
-          <Button type="primary" htmlType="submit">
-            Lưu
-          </Button>
-        </Spin>
-      </Form.Item>
-    </Form>
+    //           return Promise.reject(new Error('Mật khẩu không trùng!'));
+    //         },
+    //       }),
+    //     ]}
+    //   >
+    //     <Input.Password placeholder="Nhập lại mật khẩu" />
+    //   </Form.Item>
+    //   <Form.Item style={{ marginTop: '' }}>
+    //     <Spin spinning={false}>
+    //       <Button type="primary" htmlType="submit">
+    //         Lưu
+    //       </Button>
+    //     </Spin>
+    //   </Form.Item>
+    // </Form>
   );
 };
 
